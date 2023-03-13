04366d6d5e5c105592beb190d5768f44604c0f87
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-03-13 15:35:33 -0700

    Created initial files
    Co-authored-by: James Provance <jb24x7@gmail.com>

---
+++ ToDoList/Migrations/20230313205326_changeDueDateBackToDateTime2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Migrations
+{
+    public partial class changeDueDateBackToDateTime2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DueDate",
+                table: "Items",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "DueDate",
+                table: "Items",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+        }
+    }
+}
